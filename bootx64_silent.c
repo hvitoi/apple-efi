cd35261477709bd46c72781b8783da07ac648469
Henrique Vitoi <44772383+hvitoi@users.noreply.github.com>
2022-11-29 20:25:04 -0300

    format

--- bootx64_silent.c
+++ bootx64_silent.c
@@ -6,168 +6,174 @@
 #include "include/int_dpath.h"
 #include "include/pci_db.h"
 
-
-#define APPLE_SET_OS_VENDOR  "Apple Inc."
+#define APPLE_SET_OS_VENDOR "Apple Inc."
 #define APPLE_SET_OS_VERSION "Mac OS X 10.15"
 
 #pragma pack(1)
-typedef struct {
-  UINT8   Desc;
-  UINT16  Len;
-  UINT8   ResType;
-  UINT8   GenFlag;
-  UINT8   SpecificFlag;
-  UINT64  AddrSpaceGranularity;
-  UINT64  AddrRangeMin;
-  UINT64  AddrRangeMax;
-  UINT64  AddrTranslationOffset;
-  UINT64  AddrLen;
+typedef struct
+{
+    UINT8 Desc;
+    UINT16 Len;
+    UINT8 ResType;
+    UINT8 GenFlag;
+    UINT8 SpecificFlag;
+    UINT64 AddrSpaceGranularity;
+    UINT64 AddrRangeMin;
+    UINT64 AddrRangeMax;
+    UINT64 AddrTranslationOffset;
+    UINT64 AddrLen;
 } EFI_ACPI_ADDRESS_SPACE_DESCRIPTOR;
 
-typedef struct {
-   UINT16  VendorId;
-   UINT16  DeviceId;
-   UINT16  Command;
-   UINT16  Status;
-   UINT8   RevisionId;
-   UINT8   ClassCode[3];
-   UINT8   CacheLineSize;
-   UINT8   PrimaryLatencyTimer;
-   UINT8   HeaderType;
-   UINT8   Bist;
+typedef struct
+{
+    UINT16 VendorId;
+    UINT16 DeviceId;
+    UINT16 Command;
+    UINT16 Status;
+    UINT8 RevisionId;
+    UINT8 ClassCode[3];
+    UINT8 CacheLineSize;
+    UINT8 PrimaryLatencyTimer;
+    UINT8 HeaderType;
+    UINT8 Bist;
 } PCI_COMMON_HEADER;
- 
-typedef struct {
-   UINT32  Bar[6];               // Base Address Registers
-   UINT32  CardBusCISPtr;        // CardBus CIS Pointer
-   UINT16  SubVendorId;          // Subsystem Vendor ID
-   UINT16  SubSystemId;          // Subsystem ID
-   UINT32  ROMBar;               // Expansion ROM Base Address
-   UINT8   CapabilitiesPtr;      // Capabilities Pointer
-   UINT8   Reserved[3];
-   UINT32  Reserved1;
-   UINT8   InterruptLine;        // Interrupt Line
-   UINT8   InterruptPin;         // Interrupt Pin
-   UINT8   MinGnt;               // Min_Gnt
-   UINT8   MaxLat;               // Max_Lat
+
+typedef struct
+{
+    UINT32 Bar[6];         // Base Address Registers
+    UINT32 CardBusCISPtr;  // CardBus CIS Pointer
+    UINT16 SubVendorId;    // Subsystem Vendor ID
+    UINT16 SubSystemId;    // Subsystem ID
+    UINT32 ROMBar;         // Expansion ROM Base Address
+    UINT8 CapabilitiesPtr; // Capabilities Pointer
+    UINT8 Reserved[3];
+    UINT32 Reserved1;
+    UINT8 InterruptLine; // Interrupt Line
+    UINT8 InterruptPin;  // Interrupt Pin
+    UINT8 MinGnt;        // Min_Gnt
+    UINT8 MaxLat;        // Max_Lat
 } PCI_DEVICE_HEADER;
- 
-typedef struct {
-   UINT32  CardBusSocketReg;     // Cardus Socket/ExCA Base Address Register
-   UINT8   CapabilitiesPtr;      // 14h in pci-cardbus bridge.
-   UINT8   Reserved;
-   UINT16  SecondaryStatus;      // Secondary Status
-   UINT8   PciBusNumber;         // PCI Bus Number
-   UINT8   CardBusBusNumber;     // CardBus Bus Number
-   UINT8   SubordinateBusNumber; // Subordinate Bus Number
-   UINT8   CardBusLatencyTimer;  // CardBus Latency Timer
-   UINT32  MemoryBase0;          // Memory Base Register 0
-   UINT32  MemoryLimit0;         // Memory Limit Register 0
-   UINT32  MemoryBase1;
-   UINT32  MemoryLimit1;
-   UINT32  IoBase0;
-   UINT32  IoLimit0;             // I/O Base Register 0
-   UINT32  IoBase1;              // I/O Limit Register 0
-   UINT32  IoLimit1;
-   UINT8   InterruptLine;        // Interrupt Line
-   UINT8   InterruptPin;         // Interrupt Pin
-   UINT16  BridgeControl;        // Bridge Control
+
+typedef struct
+{
+    UINT32 CardBusSocketReg; // Cardus Socket/ExCA Base Address Register
+    UINT8 CapabilitiesPtr;   // 14h in pci-cardbus bridge.
+    UINT8 Reserved;
+    UINT16 SecondaryStatus;     // Secondary Status
+    UINT8 PciBusNumber;         // PCI Bus Number
+    UINT8 CardBusBusNumber;     // CardBus Bus Number
+    UINT8 SubordinateBusNumber; // Subordinate Bus Number
+    UINT8 CardBusLatencyTimer;  // CardBus Latency Timer
+    UINT32 MemoryBase0;         // Memory Base Register 0
+    UINT32 MemoryLimit0;        // Memory Limit Register 0
+    UINT32 MemoryBase1;
+    UINT32 MemoryLimit1;
+    UINT32 IoBase0;
+    UINT32 IoLimit0; // I/O Base Register 0
+    UINT32 IoBase1;  // I/O Limit Register 0
+    UINT32 IoLimit1;
+    UINT8 InterruptLine;  // Interrupt Line
+    UINT8 InterruptPin;   // Interrupt Pin
+    UINT16 BridgeControl; // Bridge Control
 } PCI_CARDBUS_HEADER;
- 
-typedef union {
-   PCI_DEVICE_HEADER   Device;
-   PCI_CARDBUS_HEADER  CardBus;
+
+typedef union
+{
+    PCI_DEVICE_HEADER Device;
+    PCI_CARDBUS_HEADER CardBus;
 } NON_COMMON_UNION;
- 
-typedef struct {
-   PCI_COMMON_HEADER Common;
-   NON_COMMON_UNION  NonCommon;
-   UINT32            Data[48];
+
+typedef struct
+{
+    PCI_COMMON_HEADER Common;
+    NON_COMMON_UNION NonCommon;
+    UINT32 Data[48];
 } PCI_CONFIG_SPACE;
 #pragma pack(0)
 
-
-
-
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
     EFI_STATUS Status;
 
-    EFI_BOOT_SERVICES* BS = SystemTable->BootServices;
+    EFI_BOOT_SERVICES *BS = SystemTable->BootServices;
     // get apple_set_os protocol
-    EFI_HANDLE* AppleSetOsHandleBuf;
+    EFI_HANDLE *AppleSetOsHandleBuf;
     UINTN AppleSetOsHandleCount = 0;
 
     EFI_GUID apple_set_os_guid = APPLE_SET_OS_GUID;
     Status = BS->LocateHandleBuffer(
-        ByProtocol, 
-        &apple_set_os_guid, 
-        NULL, 
+        ByProtocol,
+        &apple_set_os_guid,
+        NULL,
         &AppleSetOsHandleCount,
-        &AppleSetOsHandleBuf
-    );
-    
+        &AppleSetOsHandleBuf);
 
     // find and load bootx64_original.efi
-    EFI_LOADED_IMAGE_PROTOCOL* LoadedImage;
-	EFI_DEVICE_PATH* DevicePath = NULL;
-	EFI_HANDLE DriverHandle;
+    EFI_LOADED_IMAGE_PROTOCOL *LoadedImage;
+    EFI_DEVICE_PATH *DevicePath = NULL;
+    EFI_HANDLE DriverHandle;
 
     EFI_GUID efi_loaded_image_protocol_guid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
-    Status = BS->HandleProtocol(ImageHandle, &efi_loaded_image_protocol_guid, (VOID**) &LoadedImage);
-    if (EFI_ERROR(Status) || LoadedImage == NULL) {
+    Status = BS->HandleProtocol(ImageHandle, &efi_loaded_image_protocol_guid, (VOID **)&LoadedImage);
+    if (EFI_ERROR(Status) || LoadedImage == NULL)
+    {
         goto halt;
     }
 
     DevicePath = _INT_FileDevicePath(
-        BS, 
-        LoadedImage->DeviceHandle, 
-        L"\\EFI\\Boot\\bootx64_original.efi"
-    );
+        BS,
+        LoadedImage->DeviceHandle,
+        L"\\EFI\\Boot\\bootx64_original.efi");
 
-    if (DevicePath == NULL) {
+    if (DevicePath == NULL)
+    {
         goto halt;
-	}
+    }
 
     // Attempt to load the driver.
-	Status = BS->LoadImage(FALSE, ImageHandle, DevicePath, NULL, 0, &DriverHandle);
+    Status = BS->LoadImage(FALSE, ImageHandle, DevicePath, NULL, 0, &DriverHandle);
     _INT_FreePool(BS, DevicePath);
     DevicePath = NULL;
 
-	if (EFI_ERROR(Status)) {
-		goto halt;
-	}
+    if (EFI_ERROR(Status))
+    {
+        goto halt;
+    }
 
-	Status = BS->OpenProtocol(
-        DriverHandle, 
+    Status = BS->OpenProtocol(
+        DriverHandle,
         &efi_loaded_image_protocol_guid,
-		(VOID**)&LoadedImage, 
-        ImageHandle, 
-        NULL, 
-        EFI_OPEN_PROTOCOL_GET_PROTOCOL
-    );
-	if (EFI_ERROR(Status)) {
-		goto halt;
-	}
+        (VOID **)&LoadedImage,
+        ImageHandle,
+        NULL,
+        EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+    if (EFI_ERROR(Status))
+    {
+        goto halt;
+    }
 
     // load apple_set_os
-    for(UINTN i = 0; i < AppleSetOsHandleCount; i++) {
-        EFI_APPLE_SET_OS_IFACE* SetOsIface = NULL;
+    for (UINTN i = 0; i < AppleSetOsHandleCount; i++)
+    {
+        EFI_APPLE_SET_OS_IFACE *SetOsIface = NULL;
 
         Status = BS->OpenProtocol(
             AppleSetOsHandleBuf[i],
             &apple_set_os_guid,
-            (VOID**)&SetOsIface,
+            (VOID **)&SetOsIface,
             ImageHandle,
             NULL,
-            EFI_OPEN_PROTOCOL_GET_PROTOCOL
-        );
+            EFI_OPEN_PROTOCOL_GET_PROTOCOL);
 
-        if (EFI_ERROR(Status)) {
-        } else {
-            if (SetOsIface->Version != 0){
-                Status = SetOsIface->SetOsVendor((CHAR8*) APPLE_SET_OS_VENDOR);
-                Status = SetOsIface->SetOsVersion((CHAR8*) APPLE_SET_OS_VERSION);
+        if (EFI_ERROR(Status))
+        {
+        }
+        else
+        {
+            if (SetOsIface->Version != 0)
+            {
+                Status = SetOsIface->SetOsVendor((CHAR8 *)APPLE_SET_OS_VENDOR);
+                Status = SetOsIface->SetOsVersion((CHAR8 *)APPLE_SET_OS_VERSION);
             }
         }
     }
@@ -176,7 +182,8 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 
     // Load was a success - attempt to start the driver
     Status = BS->StartImage(DriverHandle, NULL, NULL);
-    if (EFI_ERROR(Status)) {
+    if (EFI_ERROR(Status))
+    {
         goto halt;
     }
 
@@ -184,8 +191,10 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
     return EFI_UNSUPPORTED;
 
 halt:
-    while (1) { 
-        for (UINT16 j = 0; j < 4000; j++) {
+    while (1)
+    {
+        for (UINT16 j = 0; j < 4000; j++)
+        {
             BS->Stall(10000);
         }
     }
